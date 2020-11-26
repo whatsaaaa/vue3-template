1e0cb662e52c0c6abeba5018768568c125bbf9cf
Aleksandar Milicevic <aleksandarmilicevic18@gmail.com>
2020-11-26 22:24:35 +0100

    Started working on navigation components

---
+++ src/components/NavBarRoutes.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: 'NavBarRoutes',
+  props: {
+    routes: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
+
+<template>
+  <div>Hi</div>
+</template>
---
+++ src/components/TheNavBar.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  data() {
+    return {
+      persistentNavRoutes: [
+        {
+          name: 'home',
+          title: 'Home'
+        },
+        {
+          name: 'about',
+          title: 'About'
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<template>
+  <ul class="theNavBar" />
+</template>
