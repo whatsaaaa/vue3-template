c368e31fb23bf6d518ddb4176fbcbf7d5df39fed
Aleksandar Milicevic <aleksandarmilicevic18@gmail.com>
2020-11-26 21:52:31 +0100

    Added app.config.json && and changed the App.vue content

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
-  <div id="nav">
-    <router-link to="/">
-      Home
-    </router-link>
+  <div id="app">
+    <!--
+    By binding the `key` to `$route.fullPath`, we are
+    forcing the "replacement" of the RouterView component,
+    event when routes are using the same component. With
+    this approach it will treat them as distinct and create 
+    the component again.
+    -->
+    <RouterView :key="$route.fullPath" />
   </div>
-  <router-view />
 </template>
 
+<!-- This should generally be the only global CSS in the app. -->
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
