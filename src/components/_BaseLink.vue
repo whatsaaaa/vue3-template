249f5d9c009ccb7837d9c3e1385fb3dd8c8bbb24
Aleksandar Milicevic <aleksandarmilicevic18@gmail.com>
2020-11-26 22:29:37 +0100

    BaseLink component

---
+++ src/components/_BaseLink.vue
@@ -0,0 +1,55 @@
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    href: {
+      type: String,
+      default: ''
+    },
+    allowInsecure: {
+      type: Boolean,
+      default: false
+    },
+    to: {
+      type: Object,
+      default: null
+    },
+    name: {
+      type: String,
+      default: ''
+    },
+    params: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  computed: {
+    routerLinkTo({ name, params }) {
+      return {
+        name,
+        params,
+        ...(this.to || {})
+      };
+    }
+  }
+};
+</script>
+
+<template>
+  <a
+    v-if="href"
+    :href="href"
+    target="_blank"
+    v-bind="$attrs"
+  >
+    <slot />
+  </a>
+
+  <RouterLink
+    v-else
+    :to="routerLinkTo"
+    v-bind="$attrs"
+  >
+    <slot />
+  </RouterLink>
+</template>
