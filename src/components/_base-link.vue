73d66d64f3a2f5822e047f4c027ee94642e925c6
Aleksandar Milicevic <aleksandarmilicevic18@gmail.com>
2020-11-27 12:56:10 +0100

    More complex prop validation for BaseLink

--- src/components/_base-link.vue
+++ src/components/_base-link.vue
@@ -32,6 +32,41 @@ export default {
       };
     },
   },
+  created() {
+    this.validateProps();
+  },
+  methods: {
+    // Perform more complex prop validations
+    validateProps() {
+      if (process.env.NODE_ENV === "production") return;
+
+      if (this.href) {
+        // Check for non-external URL in href.
+        if (!/^\w+:/.test(this.href)) {
+          return console.warn(
+            `Invalid <BaseLink> href: ${this.href}.\nIf you're trying to link to a local URL, provide at least a name or to`
+          );
+        }
+        // Check for insecure URL in href.
+        if (!this.allowInsecure && !/^(https|mailto|tel):/.test(this.href)) {
+          return console.warn(
+            `Insecure <BaseLink> href: ${this.href}.\nWhen linking to external sites, always prefer https URLs. If this site does not offer SSL, explicitly add the allow-insecure attribute on <BaseLink>.`
+          );
+        }
+      } else {
+        // Check for insufficient props.
+        if (!this.name && !this.to) {
+          return console.warn(
+            `Invalid <BaseLink> props:\n\n${JSON.stringify(
+              this.$props,
+              null,
+              2
+            )}\n\nEither a \`name\` or \`to\` is required for internal links, or an \`href\` for external links.`
+          );
+        }
+      }
+    },
+  },
 };
 </script>
 
