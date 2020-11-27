727848dee9ebfdf3ca283f0e247f1aef0db51084
Aleksandar Milicevic <aleksandarmilicevic18@gmail.com>
2020-11-27 13:40:54 +0100

    Dispatch action for all modules

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore } from "vuex";
+import dispatchActionForAllModules from "../utils/dispatch-action-for-all-modules";
 
 import modules from "./modules";
 
@@ -6,3 +7,7 @@ export default createStore({
   modules: modules,
   strict: process.env.NODE_ENV !== "production",
 });
+
+// Automatically run the `init` action for every module,
+// if one exists.
+dispatchActionForAllModules("init");
