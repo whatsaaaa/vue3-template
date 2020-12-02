ab486844f770a8253544cf7ef6265d6e60ef8b40
Aleksandar Milicevic <aleksandarmilicevic18@gmail.com>
2020-12-02 15:32:37 +0100

    Form submit button with nice UX

---
+++ src/components/form-submit-button.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  props: {
+    clicked: {
+      type: Boolean,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <BaseButton :disabled="clicked" type="submit">
+    <BaseIcon v-if="clicked" name="sync" spin />
+    <span v-else><slot /></span>
+  </BaseButton>
+</template>
